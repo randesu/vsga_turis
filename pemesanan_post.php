148f69aeaa062caca492265391c352d379897938
randesu <androinimesubs@gmail.com>
2024-05-21 13:26:16 +0700

    update

---
+++ pemesanan_post.php
@@ -0,0 +1,20 @@
+<?php
+ 
+
+include("koneksi.php");
+
+$nama = $_POST['nama_pemesan'];
+$nomor = $_POST['nomor_hp'];
+$tanggal_perjalanan = $_POST['tanggal_perjalanan'];
+$totalorang = $_POST['jumlah_orang'];
+
+echo $nama;
+echo $nomor;
+echo $tanggal_perjalanan;
+echo $totalorang;
+
+
+
+
+
+?>
